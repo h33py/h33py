a7ee0a479ae8a142210eac90417d02c607f18a0c
h33py <58337417+h33py@users.noreply.github.com>
2022-05-09 17:21:19 +0900

    Add files via upload

---
+++ 202203194장정경.py
@@ -0,0 +1,44 @@
+#202203194 장정경
+
+def xy_Average() :
+    x = random.randrange(0,0)
+    y = random.randrange(0,0)
+
+while True :
+        print("---------------------")
+        print(" 1 : 채점            ")
+        print(" 2 : 10개의 변수     ")
+        print(" 3 : 구간 합 구하기  ")
+        print(" 4 : 소감문          ")
+        print(" 5 : 종료            ")
+        print("---------------------")
+        sel = input("메뉴 선택 : ")
+        if   (sel == "1") :
+            print("")
+            if hap == 0 :
+                print("")
+            else :
+                print("")
+
+        elif (sel == "2") :            
+            print("")
+            
+
+        elif (sel == "3") :
+            print("구간 합 구하기")
+
+        elif (sel == "4") :
+            print("2022194 장정경 파이썬 어려워요 ㅠㅠ 열심히 하겠습니다.")
+
+        elif (sel == "5") :
+            print("bye")
+            exit(1)
+        else :
+            continue
+
+        stu_num == 3
+
+
+        
+        
+
